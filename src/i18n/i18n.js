c26101504166b5ab88bddbee3c6e3c272501117f
elchinhumbatov <elchinhumbatov@yahoo.com>
2021-09-18 21:13:33 +0400

    fixed i18n

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,14 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
+// import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import az from "../assets/locales/az/translation.json";
+import ru from "../assets/locales/ru/translation.json";
+import en from "../assets/locales/en/translation.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  az: {
+    translation: az,
+  },
+  ru: {
+    translation: ru,
+  },
+};
 
 i18n
-  .use(HttpApi)
+  // .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init(
     {
+      resources,
       fallbackLng: "az",
       detection: {
         order: ["cookie", "localStorage", "htmlTag", "subdomain"],
@@ -17,7 +33,7 @@ i18n
       // backend: {
       //   loadPath: '../assets/langs/{{lng}}/translation.json',
       // },
-      react: { useSuspense: false },
+      // react: { useSuspense: false },
       interpolation: { escapeValue: false },
     },
     (error, t) => {
